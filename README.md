98a0629dcdac7a27cef1e598beb5559861ea1ae5
Nick Saretzky <nick@saretzky.com>
2014-12-13 12:55:13 -0500

    Initial implementation, test and readme.

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
 tag.js
 ======
 
-Ultralight DOM wrapper for templating
+Ultralight DOM wrapper for templating.
+
+h1. Create a tag
+
+<pre>
+tag("h1#welcome.heading")
+</pre>
+
+Tag uses a css-selector inspired syntax to quickly set tag name, class list and id.
+
+h2. Properties
+
+Add class names:
+
+<pre>
+tag('a').class('link')
+</pre>
+
+Set attributes:
+
+<pre>
+tag('a').attr({src: "#fragment"})
+</pre>
+
+h2. Content
+
+Add text or html
+
+<pre>
+tag('p').text("1 is > 2.").html(" <strong>really.</strong>")
+</pre>
+
+Add more complex content
+
+<pre>
+tag('p')
+	.then(function() {
+		return tag('strong').text('hello')
+	})
+	.then(function() {
+		return "<p>foobar</p>"
+	})
+	.then(function() {
+		return document.getElementById('foo').cloneNode(true)
+	})
+</pre>
+
+h2. Write to the body tag
+
+tag('p').text("Hello world").close()
---
+++ src/tag.js
@@ -0,0 +1,68 @@
+(function() {
+	function tag(selector) {
+		return new Tag(selector);
+	}
+
+	function Tag(selector) {
+		var classList = [], id = null;
+		var nodeName = selector.replace(/([#.])(\w+)/g, function(match, specifier, val) {
+			if (specifier == '#') {
+				id = val;
+			} else {
+				classList.push(val);
+			}
+			return '';
+		});
+
+		this.el = document.createElement(nodeName);
+		if (classList.length) this.el.className = classList.join(' ');
+		if (id) this.el.id = id;
+	}
+
+	Tag.prototype = {
+		html: function(html) {
+			this.el.innerHTML = html;
+			return this;
+		},
+		text: function(text) {
+			if (text) this.el.innerText = text;
+			return this;
+		},
+		class: function(classNames) {
+			if (classNames) this.className += ' ' + classNames;
+			return this;
+		},
+		attr: function(attrs) {
+			if (!attrs) return;
+			for (a in attrs) {
+				this.el.setAttribute(a, attrs[a]);
+			}
+			return this;
+		},
+		then: function(f) {
+			debugger;
+			if (!f) return;
+			var r = f();
+			if (r instanceof Tag) {
+				this.el.appendChild(r.el);
+			} else if (r instanceof Element) {
+				this.el.appendChild(r);
+			} else if (typeof(r) == 'string') {
+				this.innerHTML += r;
+			}
+			return this;
+		},
+		data: function(data) {
+			if (!data) return;
+			for (var p in data) {
+				this.el.dataset[p] = data[p];
+			}
+			return this;
+		},
+		close: function() {
+			document.body.appendChild(this.el);
+		}
+	}
+
+	window.tag = tag;
+})();
